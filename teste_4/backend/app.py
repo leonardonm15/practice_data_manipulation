4417284444302d7323cd64b93759481f2afb6d24
Leonardo Nunes Muniz <leonardonm15@gmail.com>
2022-06-01 16:40:23 -0300

    api done

---
+++ teste_4/backend/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import pandas as pd
+
+file_path = r'C:\data\Relatorio_cadop teste 4.csv'
+
+df = pd.read_csv(file_path,
+                 encoding='LATIN-1',
+                 sep=';',
+                 engine='python',
+                 on_bad_lines='skip',
+                 skiprows=[0, 1])  # turning the csv to a data frame to modify it
+
+cols  = df.columns
+df.columns = cols.map(lambda x: x.replace(' ', '_') if isinstance(x, (str)) else x) #remove the bad characters from the columns names
+
+app = Flask(__name__)
+ 
+
+# whenever it recieves the input by the url it searches the finds strings which the substring op_name is contained
+@app.route('/get/<string:op_name>', methods = ['GET'])
+def get_names(op_name):
+    op_name = op_name.upper()
+    df2 = df.loc[df['Razão_Social'].str.contains(op_name)].head()
+    df2 = df2['Razão_Social']
+    return df2.to_dict()
+
+if __name__ == "__main__":
+    app.run(debug=True)
